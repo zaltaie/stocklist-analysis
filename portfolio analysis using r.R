484a1f358a114872afb55c30d8b8312ea791c11a
zaltaie <46233852+zaltaie@users.noreply.github.com>
2020-03-13 15:28:36 -0700

    Update portfolio analysis using r.R

--- portfolio analysis using r.R
+++ portfolio analysis using r.R
@@ -6,9 +6,9 @@ library(pROC)
 
 getSymbols("AMD")
 chartSeries(AMD, 
-			subset = 'last 6 months',
-			up.col = 'green',
-			dn.col = 'red')
+		subset = 'last 6 months',
+		up.col = 'green',
+		dn.col = 'red')
 addBBands(n = 20, 
 		  sd = 2, 
 		  maType = "SMA", 
@@ -16,16 +16,16 @@ addBBands(n = 20,
 		  on = -1)
 
 tickers <- c("AAPL", "ADBE", "AMZN", "SNAP", "AMD", 
-					   "TWTR", "GOOGL", "CRM", "EBAY", "FB", "INTL", 
-					   "MSFT", "MU", "NFLX", "SBUX", "TSLA", "VOD", 
-					   "DIS", "BA", "NVDA", "LULU", "SQ", "PLUG")
+	 "TWTR", "GOOGL", "CRM", "EBAY", "FB", "INTL", 
+	 "MSFT", "MU", "NFLX", "SBUX", "TSLA", "VOD", 
+	  "DIS", "BA", "NVDA", "LULU", "SQ", "PLUG")
 getSymbols(tickers)
 
 # merge them all together
 portf <- data.frame(as.xts(merge(AAPL, ADBE, AMZN, SNAP, AMD, 
-									 TWTR, GOOGL, CRM, EBAY, FB, INTL, 
-									 MSFT, MU, NFLX, SBUX, TSLA, VOD, 
-									 DIS, BA, NVDA, LULU, SQ, PLUG)))
+				 TWTR, GOOGL, CRM, EBAY, FB, INTL, 
+				 MSFT, MU, NFLX, SBUX, TSLA, VOD, 
+				 DIS, BA, NVDA, LULU, SQ, PLUG)))
 head(portf[,1:12],2)
 
 outcome_symbol <- 'FISV.Volume'
