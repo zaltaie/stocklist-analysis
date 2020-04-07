8090a67edb3cc05e103ed426396ee640386f3b65
zaltaie <46233852+zaltaie@users.noreply.github.com>
2020-04-06 22:44:44 -0700

    Add files via upload

---
+++ k_cluster.R
@@ -0,0 +1,34 @@
+setwd("~/Desktop/R project/Stock Finance")
+
+
+library(quantmod)
+library(PerformanceAnalytics)
+library(magrittr)
+library(tseries)
+
+start_time <- Sys.time()
+
+tradingData <- read.csv("NYSE.csv", header = TRUE)
+
+str(tradingData)
+
+head(tradingData, n=5)
+
+colnames(tradingData) <- c("ID","OPEN_P", "HIGH_P", "LOW_P", "CLOSE_P", "VOLUME", "CLOSE_ADJ_P")
+
+#drop ID column
+tradingData <- tradingData[, -c(1)]
+
+#standardize the continuous numbers
+tradingData <- scale(tradingData)
+
+#perform kmeans with k = 4
+fit <- kmeans(tradingData, centers = 4, iter.max = 10000)
+
+#display the results, taken from the fit table centers
+fit$centers
+View(fit$centers)
+
+end_time <- Sys.time()
+total_time <- end_time - start_time
+total_time
